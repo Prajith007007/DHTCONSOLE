39ef9b3b672639b493fff584219341eecb444906
Kuntal08 <kuntal.majee1998@gmail.com>
2020-04-24 12:32:52 +0530

    Linking the File_upload with encryption.py python script

---
+++ Fodgers_DHT-master/utils/file_upload.php
@@ -0,0 +1,14 @@
+<?php
+    if(isset($_POST['uploadbutton'])) {
+
+      $file_name = $_FILES['file']['name'];
+      $file_type = $_FILES['file']['type'];
+      $file_size = $_FILES['file']['size'];
+      $file_tem_loc = $_FILES['file']['tmp_name'];
+      $file_store = "deploy/".$file_name;
+
+      move_uploaded_file($file_tem_loc, $file_store);
+    }
+    $result = shell_exec("python encryption.py");
+    echo $result;
+?>
